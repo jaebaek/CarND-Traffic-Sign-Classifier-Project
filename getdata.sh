a82399a08dc94a8aaa1fc88ed89bac74954c64b2
Jaebaek Seo <jaebaek@kaist.ac.kr>
2018-02-24 14:54:36 +0000

    [DONE] code

---
+++ getdata.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+wget http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Test_Images.zip
+unzip GTSRB_Final_Test_Images.zip
+
+wget http://benchmark.ini.rub.de/Dataset/GTSRB_Final_Test_GT.zip
+unzip GTSRB_Final_Test_GT.zip
+
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/February/5898cd6f_traffic-signs-data/traffic-signs-data.zip
+unzip traffic-signs-data.zip
